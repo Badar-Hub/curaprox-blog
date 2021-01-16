5138b200068ca92338aa9932950110aafbe450d2
Badar <bapk65@gmail.com>
2021-01-17 03:09:27 +0500

    wip

--- .history/src/router/index_20210117024753.js
+++ .history/src/router/index_20210117024753.js
@@ -18,8 +18,8 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
-    path: '/braces',
-    name: 'Braces',
+    path: '/cs-5460',
+    name: 'Cs5460',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.

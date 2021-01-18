1f158e315e4214d654a1c4d476dcaa9221f9fa8a
Badar Abdullah <bapk65@gmail.com>
2021-01-18 00:51:56 +0000

    wip

---
+++ src/views/TeethWhitening.vue
@@ -0,0 +1,473 @@
+<template>
+  <div>
+    <Header />
+    <div class="row justify-center home q-pb-xl">
+      <div class="blog-header ">
+        <img src="@/assets/img/cover/need-post-cleen-teeth-1200x500.jpg" />
+        <div class="row justify-center">
+          <h4 class="q-my-sm" style="color: #FF8484">Teeth, Care</h4>
+        </div>
+        <p class="q-pa-sm text-h3 text-center	text-weight-regular">
+          TEETH WHITENING TOOTHPASTE
+        </p>
+        <br />
+      </div>
+      <p class="text-h5 text-center text-weight-regular">
+        If you’re unhappy about the colour of your teeth, it’s now possible to
+        reverse
+        <br />
+        years of staining and enjoy the confidence of whiter, brighter,
+        fresher-looking<br />
+        teeth.
+        <br />
+        But it’s really important to make sure you whiten your teeth in a safe
+        way that doesn’t harm your teeth or your oral health.
+      </p>
+    </div>
+    <div class="row home blog">
+      <div class="col col-md-3 q-pr-md padding">
+        <h3>Why this guide?</h3>
+        <br />
+        <h3>The best teeth whitening toothpaste</h3>
+        <br />
+        <h3>Whitening toothpaste that works - gently and effectively</h3>
+        <br />
+        <h3>Activated charcoal teeth whitening toothpaste for effective tooth care</h3>
+        <br />
+        <h3>How to brush your teeth</h3>
+        <br />
+        
+      </div>
+      <div class="col col-md-9 q-pl-md q-pb-xl">
+        <div class="text-left desctxt" style="display: block;">
+          <h2>IS WHITENING YOUR TEETH BAD?</h2>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-whitening-bad-576x540-web.png"
+              alt=""
+            />
+          
+          <p>&nbsp;</p>
+          <h3>The difference between teeth whitening and teeth bleaching</h3>
+          <p>
+            The first thing to understand is the difference between teeth
+            whitening and teeth bleaching.<br /><br />Teeth bleaching is usually
+            carried out by a dentist – or under the supervision of a dentist –
+            using bleach gels or products. Home bleaching kits are also
+            available.<br /><br />Bleaching uses a chemical process to make the
+            teeth whiter than they ever have been previously.
+          </p>
+          <h3><br />The dangers of teeth bleaching</h3>
+          <ul>
+            <li>
+              Your gums and oral mucosa – the tissue that lines the mouth – may
+              be sensitive to the chemicals used in bleaching… particularly if
+              you already have
+              <a
+                href="https://curaprox.ch/en/blog/post/sensitive-teeth-the-ultimate-guide"
+                ><strong>sensitive teeth</strong></a
+              >
+            </li>
+            <li>
+              Some bleaching patients say their teeth feel sensitive during or
+              after treatment. Other side effects can include discomfort in the
+              gums or a sore throat
+            </li>
+            <li>Teeth are more prone to staining after bleaching</li>
+          </ul>
+          <h3><br />Gentle, natural teeth whitening</h3>
+          <p>
+            At CURAPROX, we don’t believe in home bleaching products.<br /><br />We
+            believe teeth whitening should work towards restoring your teeth to
+            their original whiteness… in the most gentle, natural, healthy way
+            possible.<br /><br />We like white teeth… but we LOVE healthy
+            teeth.<br /><br />Whitening should never damage your gums or oral
+            mucosa.<br /><br />Oral care should never be harsh or abrasive.<br /><br />You
+            need to protect your teeth enamel. If it starts to erode, it can
+            lead to
+            <a
+              href="https://curaprox.ch/en/blog/post/sensitive-teeth-the-ultimate-guide"
+              ><strong>sensitivity and pain</strong></a
+            >.
+          </p>
+          <h3><br />Want Hollywood white teeth?</h3>
+          <p>
+            If you’re specifically looking for “Hollywood white teeth”, then
+            please visit a dentist.<br /><br />After treatment, you can use
+            CURAPROX products to keep them looking and feeling bright and
+            healthy.
+          </p>
+          <h2><br /><br />THE BEST TEETH WHITENING TOOTHPASTE</h2>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-whitening-BeYou-colors-576x540-web.jpg"
+              alt=""
+            />
+          
+          <p>&nbsp;</p>
+          <h3>Can toothpaste actually whiten teeth?</h3>
+          <p>
+            If you want whiter teeth, you no longer have to put yourself through
+            expensive, potentially harsh and painful bleaching treatment.<br /><br />There
+            are many whitening toothpastes on the market.<br /><br />But not all
+            whitening toothpastes were born equal.<br /><br />Some are more
+            effective than others.<br /><br />Some are gentler than others.<br /><br />And,
+            unfortunately, some contain harsher ingredients than others – such
+            as sodium metaphosphate, zirconium silicate and calcium
+            pyrophosphate.
+          </p>
+          <h3><br />Beware abrasive whitening toothpaste</h3>
+          <p>
+            The higher the Relative Dentin Abrasivity (RDA) score of a
+            toothpaste, the more likely it is to use these ingredients… and the
+            more likely it is to:
+          </p>
+          <ul>
+            <li>Erode the enamel on your teeth</li>
+            <li>Damage your gums and oral mucosa</li>
+            <li>
+              Lead to enhanced
+              <a
+                href="https://curaprox.ch/en/blog/post/sensitive-teeth-the-ultimate-guide"
+                ><strong>sensitivity</strong></a
+              >
+            </li>
+          </ul>
+          <p>
+            We believe effective whitening toothpastes should be gentle.<br /><br />We
+            believe they should actually care for your oral health.<br /><br />SLS,
+            Triclosan and other nasties are not what we want for you.<br /><br />We
+            want you to enjoy exceptional oral health and whiter teeth –
+            naturally.<br /><br />Which toothpaste do dentists recommend?
+          </p>
+          <p>Introducing:</p>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-related-product-toothpaste-beyou-400x250-web-new.jpg"
+              alt=""
+            />
+          
+          <h2>CURAPROX [BE YOU.]</h2>
+          <p>
+            <a
+              href="https://curaprox.ch/en/shop/toothpaste/whitening-toothpaste"
+              >Buy</a
+            >
+          </p>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-related-product-toothpaste-biw-400x250-web.jpg"
+              alt=""
+            />
+          
+          <h2>CURAPROX BLACK IS WHITE</h2>
+          <p>
+            <a
+              href="https://curaprox.ch/en/shop/toothpaste/activated-carbon-toothpaste/black-is-white-90-ml"
+              >Buy</a
+            >
+          </p>
+          <h2>
+            <br /><br />WHITENING TOOTHPASTE THAT WORKS - GENTLY AND EFFECTIVELY
+          </h2>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-whitening-BeYou-pink-576x540-web.jpg"
+              alt=""
+            />
+          
+          <p>&nbsp;</p>
+          <h3>
+            CURAPROX [BE YOU.] – Gentle everyday whitening. Zesty, fresh
+            flavours
+          </h3>
+          <ul>
+            <li>
+              <a
+                href="https://curaprox.ch/en/shop/toothpaste/whitening-toothpaste"
+                ><strong>CURAPROX [BE YOU.]</strong></a
+              >
+              is the toothpaste for gentle everyday whitening – in six zesty
+              flavours…
+            </li>
+            <li>
+              Grapefruit + bergamot, peach + apricot, watermelon, gin tonic +
+              persimmon, blackberry + liquorice, apple + aloe
+            </li>
+            <li>With beautiful herbs and natural ingredients</li>
+            <li>
+              It’s gentle whitening with glucose oxidase – a natural enzyme that
+              reacts with the sugar in the mouth to create a low concentration
+              of hydrogen peroxide in the saliva that i) whitens the teeth and
+              ii) directly combats bacteria
+            </li>
+            <li>
+              The glucose oxidase additionally takes care of your tooth enamel
+              and gums because it activates the natural lactoperoxidase system
+              of the saliva, which also has an antibacterial – as well as an
+              antiviral and antifungal – effect
+            </li>
+          </ul>
+          <p>We recommend:</p>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-related-product-toothpaste-beyou-400x250-web-new.jpg"
+              alt=""
+            />
+          
+          <h2>CURAPROX [BE YOU.]</h2>
+          <p>
+            <a
+              href="https://curaprox.ch/en/shop/toothpaste/whitening-toothpaste"
+              >Buy</a
+            >
+          </p>
+          <h2>
+            <br /><br />ACTIVATED CHARCOAL TEETH WHITENING TOOTHPASTE FOR
+            EFFECTIVE TOOTH CARE
+          </h2>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/need-whitening-biw-576x540-web.jpg"
+              alt=""
+            />
+          
+          <p>&nbsp;</p>
+          <h3>
+            CURAPROX Black Is White whitening toothpaste – Goodbye stains. Hello
+            healthy mouth…
+          </h3>
+          <p>Does charcoal toothpaste whiten teeth?</p>
+          <ul>
+            <li>
+              <a
+                href="https://curaprox.ch/en/shop/toothpaste/activated-carbon-toothpaste/black-is-white-90-ml"
+                ><strong>CURAPROX Black Is White</strong></a
+              >
+              is a black toothpaste that gently whitens with activated charcoal
+            </li>
+            <li>
+              Pure, highly-concentrated, activated carbon removes stains gently
+              – by simply absorbing unattractive, discoloured particles…
+            </li>
+            <li>… and keeps your mouth feeling fresh</li>
+            <li>
+              Now you can enjoy whiter teeth without harsh, painful chemicals…
+            </li>
+            <li>… and with a lemon-minty-fresh flavour</li>
+          </ul>
+          <p>We recommend:</p>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-related-product-toothpaste-biw-400x250-web.jpg"
+              alt=""
+            />
+          
+          <h2>CURAPROX BLACK IS WHITE</h2>
+          <p>
+            <a
+              href="https://curaprox.ch/en/shop/toothpaste/activated-carbon-toothpaste/black-is-white-90-ml"
+              >Buy</a
+            >
+          </p>
+          <h2>
+            <br /><br /><strong>HOW TO BRUSH YOUR TEETH</strong><br />&nbsp;
+          </h2>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-whitening-toothbrusgh-576x540-web.png"
+              alt=""
+            />
+          
+          <p>&nbsp;</p>
+          <p>
+            Using a gentle, effective toothbrush with&nbsp;<a
+              href="https://curaprox.ch/en/blog/post/clean-teeth-the-ultimate-guide"
+              >the best technique</a
+            >
+            – in conjunction with the best whitening toothpaste – is crucial.<br /><br />Hard
+            toothbrushes, meanwhile, can scratch the enamel, making teeth more
+            prone to stains.<br /><br />&nbsp;
+          </p>
+          <p>
+            <a href="https://youtu.be/UshVQPMoCMI">Video correct brushing</a>
+          </p>
+          <h3>
+            <br />Is the iconic CS 5460 the gentlest, most effective toothbrush
+            in the world?
+          </h3>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-whitening-cs5460-576x540-web.jpg"
+              alt=""
+            />
+          
+          <p>&nbsp;</p>
+          <p>
+            The
+            <a
+              href="https://curaprox.ch/en/shop/toothbrushes/manual-toothbrushes/cs-5460"
+              ><strong>CURAPROX CS 5460 toothbrush</strong></a
+            >:
+          </p>
+          <ul>
+            <li>
+              Features 5,460 fine and gentle CUREN® filaments (instead of
+              conventional nylon) – each 0.1mm in diameter
+            </li>
+            <li>Reaches every nook and cranny</li>
+          </ul>
+          <h3>
+            <br />CURAPROX Black Is White Hydrosonic. Carbon brush heads.
+            Effective whitening.
+          </h3>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-whitening-biw-sonic-576x540-web.jpg"
+              alt=""
+            />
+          
+          <p>&nbsp;</p>
+          <p>
+            If you prefer electric toothbrushes… try the
+            <a
+              href="https://curaprox.ch/en/shop/sonic-toothbrush/hydrosonic/black-is-white-hydrosonic"
+              ><strong
+                >CURAPROX Black Is White Hydrosonic sonic toothbrush</strong
+              ></a
+            >:
+          </p>
+          <ul>
+            <li>
+              The perfect partner for
+              <a
+                href="https://curaprox.ch/en/shop/toothpaste/activated-carbon-toothpaste/black-is-white-90-ml"
+                ><strong>CURAPROX Black Is White toothpaste</strong></a
+              >
+            </li>
+            <li>
+              The CUREN® filaments of the brush heads are made with activated
+              carbon… removing discoloured particles without affecting the
+              enamel
+            </li>
+          </ul>
+          <h3><br />And don’t forget a great oral health routine…</h3>
+          <p>
+            Because why would you take such great care over choosing the best
+            products…<br /><br />… without treating yourself to the ultimate
+            oral health care routine?
+          </p>
+          <p>
+            Here are our exceptional guides to
+            <a
+              href="https://curaprox.ch/en/blog/post/clean-teeth-the-ultimate-guide"
+              ><strong>brushing your teeth</strong></a
+            >
+            and
+            <a
+              href="https://curaprox.ch/en/blog/post/how-do-i-take-care-of-my-gums"
+              ><strong>using interdental brushes</strong></a
+            >
+          </p>
+          <p>We recommend:</p>
+          <p>&nbsp;</p>
+          <h2>CURAPROX CS 5460 TOOTHBRUSH</h2>
+          <p>
+            <a href="https://curaprox.ch/en/info/toothbrushes/cs-5460"
+              >Learn more</a
+            ><a
+              href="https://curaprox.ch/en/shop/toothbrushes/manual-toothbrushes/cs-5460"
+              >Buy</a
+            >
+          </p>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-related-product-sonic-toothbrush-biw-400x250-web.jpg"
+              alt=""
+            />
+          
+          <h2>CURAPROX BLACK IS WHITE HYDROSONIC SONIC TOOTHBRUSH</h2>
+          <p>
+            <a
+              href="https://curaprox.ch/en/info/sonic-toothbrushes/black-is-white-hydrosonic"
+              >Learn more</a
+            ><a
+              href="https://curaprox.ch/en/shop/sonic-toothbrush/hydrosonic/black-is-white-hydrosonic"
+              >Buy</a
+            >
+          </p>
+          
+            <img
+              src="https://curaprox.ch/modules/advancedcms/uploads/AdvancedCmsBlockTeaser/64/curaprox-related-product-idb-300x250.jpg"
+              alt=""
+            />
+          
+          <h2>CURAPROX CPS PRIME INTERDENTAL BRUSHES</h2>
+        </div>
+      </div>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+// @ is an alias to /src
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  max-width: 1200px;
+  margin: auto;
+}
+.headingtxt {
+  letter-spacing: 1.5;
+}
+.blog-header {
+  img {
+    max-width: fit-content;
+    width: 100%;
+  }
+}
+
+.blog {
+  h2 {
+    font-size: 50px;
+    font-weight: 400;
+  }
+  h3 {
+    font-size: 30px;
+    margin: 20px 0;
+  }
+  h4 {
+    font-size: 23px;
+    margin-top: 105px;
+    margin-bottom: 25px;
+  }
+  p,
+  li {
+    font-size: 22px;
+    font-family: sans-serif;
+  }
+
+  a {
+    font-size: 18px;
+    color: black;
+    text-decoration: none;
+  }
+  .padding {
+    padding-top: 75px;
+  }
+  img {
+    max-width: 1000px;
+    width: 100%;
+  }
+}
+</style>
